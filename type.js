b3ebf1d935c2a2dd22ab591e129740e233c8fdb4
computerscience41 <rakibhasan934541@gmail.com>
2025-01-22 10:35:17 +0600

    type of

---
+++ type.js
@@ -0,0 +1,10 @@
+// Type of variable
+var age = 30;
+var price = 40;
+var name = "john"
+var isTrue = true;
+
+console.log(typeof age);
+console.log(typeof price);
+console.log(typeof name);
+console.log(typeof isTrue);
