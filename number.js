d9260f2c9a0fd2e5c78b15901db9b8284643a26f
computerscience41 <rakibhasan934541@gmail.com>
2025-01-22 11:37:01 +0600

    number

---
+++ number.js
@@ -0,0 +1,11 @@
+var applePrice = 20;
+var orangePrice = parseFloat ("25.5");
+
+// console.log(typeof applePrice);
+// console.log(orangePrice + applePrice);
+
+var num1 = 0.1;
+var num2 = 0.2;
+var total = num1 + num2;
+
+console.log(total.toFixed(2));
