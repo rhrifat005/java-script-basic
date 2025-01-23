e239532050699ee43bc4df282e6fc89ff239ccaf
computerscience41 <rakibhasan934541@gmail.com>
2025-01-23 21:34:54 +0600

    operation

---
+++ operation.js
@@ -0,0 +1,23 @@
+var eggPrice = 20;
+var onionPrice = 10;
+
+// console.log(eggPrice + onionPrice);
+// console.log(eggPrice - onionPrice);
+// console.log(eggPrice * onionPrice);
+// console.log(eggPrice / onionPrice);
+
+var num1 = 10;
+var num2 = 3;
+console.log(num1 % num2);
+
+
+// .............................
+
+var price = 500;
+price = price + 50;
+
+console.log(price);
+
+var newPrice = price + 50;
+var discountPrice = newPrice /100 * 10; 
+console.log(discountPrice);
